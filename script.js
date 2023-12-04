487751eb4907d33632012b2cf71085ad4367eb66
Samuel VIVIER <samvivier25@gmail.com>
2023-12-04 08:56:59 +0100

    modification du script

--- script.js
+++ script.js
@@ -10,18 +10,17 @@ form.addEventListener("submit", (e) => {
     let lastName = document.getElementById("last-name").value;
     let message = document.getElementById("message").value;
     
-    
-    //Les mettres les champs dans une balise div avec la classe adapté
-    const newComment = document.createElement("div");
-    newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
-    
     // si les formulaires ne sont pas complet on met un messeage d'erreur.
     if(!firstName || !lastName || !message){
         document.getElementById("error-message").style.display = "block";
     }else{
         // Efface le message d'érreur si les condition son bonne.
         document.getElementById("error-message").style.display = "none";
-    //  h3 pour le nom et prenom  et un balise <div> puis <p> pour le commentaire avec les bonnes classes.
+
+        //Les mettres les champs dans une balise div avec la classe adapté
+        const newComment = document.createElement("div");
+        newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
+        //h3 pour le nom et prenom  et un balise <div> puis <p> pour le commentaire avec les bonnes classes.
         newComment.innerHTML = `
     <div class="flex-1 py-10 border-t border-gray-200">
     <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
@@ -30,11 +29,11 @@ form.addEventListener("submit", (e) => {
     </div>
     `;
     
-    //Envoyer dans les commentaires
+    //Envoyer dans les commentaires.
     let commentsList = document.getElementById("comment-list")
     commentsList.appendChild(newComment);
     
-    //supprimer le champ du contenue une fois validé 
+    //supprimer le champ du contenue une fois validé.
     document.getElementById("first-name").value = "";
     document.getElementById("last-name").value = "";
     document.getElementById("message").value = "";
